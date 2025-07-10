8e0f1bbf659a46c7f3eb70a3dd7b1cd7f9241a32
sandunudayakantha <sndnudayakantha@gmail.com>
2025-07-10 13:30:40 +0530

    db created

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,25 +1,22 @@
-import { Stack } from "expo-router";
-import { ClerkLoaded, ClerkProvider } from '@clerk/clerk-expo'
-import { tokenCache } from '@clerk/clerk-expo/token-cache'
+
+
+
 import { SafeAreaProvider,SafeAreaView } from "react-native-safe-area-context";
-import login from "./(auth)/login";
+
 import InitialLayout from "@/components/InitialLayout";
+import ClerkAndConvexProvider from "@/providers/ClerkAndConvexProvider";
 
-const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
 
-if (!publishableKey) {
-  throw new Error('Missing publishable key')
-}
 
 export default function RootLayout() {
   return (
-<ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey} >
-  <ClerkLoaded>
+<ClerkAndConvexProvider>
     <SafeAreaProvider>
       <SafeAreaView style={{flex:1, backgroundColor:"black"}}>
          <InitialLayout/>
         </SafeAreaView>
     </SafeAreaProvider>  
-  </ClerkLoaded>
-</ClerkProvider>
+
+</ClerkAndConvexProvider>    
+  
 );}
---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+
+
+export default {
+  providers: [
+    {
+      domain: "https://dynamic-terrier-8.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
